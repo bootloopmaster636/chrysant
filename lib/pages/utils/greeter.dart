21214e280c05e1a38b074bb81a12bc6b2d1a4c9b
bootloopmaster636 <christhartono@hotmail.com>
2024-01-12 05:46:47 +0700

    add quotes generator

--- lib/pages/utils/greeter.dart
+++ lib/pages/utils/greeter.dart
@@ -1,4 +1,6 @@
-String greeter() {
+import 'dart:math';
+
+String timeGreeter() {
   final int hour = DateTime.now().hour;
 
   if (hour >= 0 && hour < 5) {
@@ -14,4 +16,24 @@ String greeter() {
   } else {
     return 'Hello';
   }
+}
+
+String greeter() {
+  final List<String> quotes = <String>[
+    'Wishing you a day filled with joy and positivity.',
+    'May your day be as bright as your smile.',
+    'Hope your day is as amazing as you are.',
+    'May today bring you happiness and success.',
+    'Wishing you a wonderful day filled with laughter and love.',
+    "Wishing you a day that's as awesome as you are.",
+    'May your day be as fantastic as your dreams.',
+  ];
+
+  final Random random = Random();
+
+  // on rare occasion, lets return an easter egg :)
+  if (random.nextInt(100) == 42) return 'These quotes are computer generated!';
+
+  final int randomNumber = random.nextInt(quotes.length);
+  return quotes[randomNumber];
 }
