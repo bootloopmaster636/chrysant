424c6a830a782f146422831a7eede1bcda45b608
bootloopmaster636 <christhartono@hotmail.com>
2024-01-12 05:26:11 +0700

    fix greeter

--- lib/pages/utils/greeter.dart
+++ lib/pages/utils/greeter.dart
@@ -1,12 +1,17 @@
 String greeter() {
   final int hour = DateTime.now().hour;
-  if (hour > 5 && hour < 11) {
-    return 'Good morning';
-  } else if (hour < 15) {
-    return 'Good afternoon';
-  } else if (hour < 18) {
-    return 'Good evening';
-  } else {
+
+  if (hour >= 0 && hour < 5) {
     return 'Good night';
+  } else if (hour >= 5 && hour < 12) {
+    return 'Good morning';
+  } else if (hour >= 12 && hour < 16) {
+    return 'Good afternoon';
+  } else if (hour >= 16 && hour < 19) {
+    return 'Good Evening';
+  } else if (hour >= 19) {
+    return 'Good night';
+  } else {
+    return 'Hello';
   }
 }
